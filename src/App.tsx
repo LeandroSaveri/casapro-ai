2d8cb0cb7dacb5f7b28a36241aff079533c7c5c7
LeandroSaveri <leandrosaveri1106@gmail.com>
2026-02-23 19:54:54 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-return (
-  <div style={{ color: "black", padding: 20 }}>
-    APP FUNCIONOU — TESTE VISUAL
-  </div>
-);
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ color: "black", padding: 20 }}>
+      APP FUNCIONOU — TESTE VISUAL
+    </div>
+  );
+}
